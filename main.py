1420ec2d23e3cd7a3294c91d4482d6a89465de77
christianfirecup <christianfirecup@aol.com>
2023-12-14 23:14:52 -0500

    removed the need to use sleep by constantly checking the completion status

--- main.py
+++ main.py
@@ -2,48 +2,46 @@ from openai import OpenAI
 import Createassistants as c
 from dotenv import load_dotenv
 import os
+
 api_key = os.getenv("OPENAI_API_KEY")
 load_dotenv()
 
 client = OpenAI(api_key=api_key)
 
-ask = input("what is your question")
-assistant = c.createas("PythonGPT", "you are a insanely good python instructor that answers any python problem in the most educational way possible ", [{"type": "code_interpreter"}], "gpt-4-1106-preview")
+ask = input("what is your question? ")
+# assistant = c.createas("PythonGPT", "you are a insanely good python instructor that answers any python problem in the most educational way possible ", [{"type": "code_interpreter"}], "gpt-4-1106-preview")
 thread = client.beta.threads.create()
 print(thread)
-print(assistant.id)
+print("asst_Sk0ZX5XZjtpV4y5CdFtjKHk6")
 
 message = client.beta.threads.messages.create(
-    thread_id= thread.id,
+    thread_id=thread.id,
     role="user",
-    content= ask
+    content=ask
 
 )
-
-
-
 
 run = client.beta.threads.runs.create(
     thread_id=thread.id,
-    assistant_id= assistant.id,
-    instructions= "You are to answer the user in the most educational way possible"
-
-)
-import time as t
-t.sleep(60)
-
-result = client.beta.threads.runs.retrieve(
-    thread_id=thread.id,
-    run_id=run.id
+    assistant_id="asst_Sk0ZX5XZjtpV4y5CdFtjKHk6",
+    instructions="You are to answer the user in the most educational way possible"
 
 )
 
-if result.status == 'completed':
-    messages = client.beta.threads.messages.list(
-        thread_id=thread.id
+received = False
 
+while not received:
+    # Retrieve the latest run status
+    result = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
     )
-    for message in messages.data:
-        role = message.role
-        content = message.content[0].text.value
-        print(f"{role.capitalize()}: {content}")
+
+    # Check if the run is completed
+    if result.status == 'completed':
+        received = True
+        messages = client.beta.threads.messages.list(thread_id=thread.id)
+        for message in messages.data:
+            role = message.role
+            content = message.content[0].text.value
+            print(f"{role.capitalize()}: {content}")
